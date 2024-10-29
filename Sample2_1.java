55bb1b34d671ea94dda1ae49b5f776b8d920c726
WoemTheCat <t111820031@ntut.org.tw>
2024-10-29 22:27:03 +0800

    Sample2_3

--- Sample2_1.java
+++ Sample2_1.java
@@ -1,6 +1,6 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class Sample2_1 {
     public static void main(String[] args) throws IOException {
